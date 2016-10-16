6d8c2cfb8f84727f65c6847d49b2200343c40aca
Craig Moreno <craig@bodyhigh.com>
2016-10-15 22:43:02 -0700

    working mvp

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var express = require("express");
+var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
-    // res.status(200).send('Hello Socket!!!');
     res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
     console.log('A user is connected');
+    socket.broadcast.emit('hi');
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
 
     socket.on('chat message', function(msg) {
-        console.log('Message: ' + msg);
-    })
-})
+        io.emit('chat message', msg);
+    });
+});
 
-http.listen(3000, function() {
+server.listen(3000, function() {
     console.log('Listening on port 3000');
 });
